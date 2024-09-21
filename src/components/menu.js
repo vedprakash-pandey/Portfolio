8f24f094cc1f144cf1e1cb00c4a03da1615cf45e
Sachin Gabhud <sachin.gabhud@netenrich.com>
2024-09-21 19:02:19 +0530

    fixed resume link issue for github pages

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Helmet } from 'react-helmet';
-import { Link } from 'gatsby';
+import { Link, withPrefix } from 'gatsby';
 import styled from 'styled-components';
 import { navLinks } from '@config';
 import { KEY_CODES } from '@utils';
@@ -266,7 +266,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
+            <a href={withPrefix('/resume.pdf')} target="_blank" className="resume-link" rel="noreferrer">
               Resume
             </a>
           </nav>
